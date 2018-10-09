d7a9573c4443686dcc1db541609e9b34477cbe76
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-10-09 20:33:14 +0200

    added another div to content

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 "use strict";
 
 function init() {
-
-    //setInterval(update, 5000); //Runs several times
-    setTimeout(update, 5000);
-
+  //setInterval(update, 5000); //Runs several times
+  setTimeout(update, 5000);
 }
 init();
 
 function update() {
-    let data = JSON.parse(FooBar.getData())
-    console.log(data);
-    handleBartenders(data.bartenders)
+  let data = JSON.parse(FooBar.getData());
+  console.log(data);
+  handleBartenders(data.bartenders);
+  handlebar(data.bar);
 }
 function handleBartenders(bartenders) {
-    console.log(bartenders)
+  console.log(bartenders);
+}
+function handlebar(bar) {
+  console.log(bar);
 }
