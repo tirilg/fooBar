e6525b9a4f8160854ade0f0c8c6013b38f5956d7
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-10-10 13:13:27 +0200

    both bar chart and doughnut chart added

---
+++ chart.js
@@ -0,0 +1,58 @@
+let cdb = document.querySelector("#myChart").getContext("2d");
+let myChart = new Chart(cdb, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [5, 8, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
+
+let cdc = document.querySelector("#clockChart");
+let clockChart = new Chart(cdc, {
+  type: "doughnut",
+  tooltipFillColor: "rgba(51, 51, 51, 0.55)",
+  data: {
+    labels: ["blueberry", "grape", "apple", "pineapple"],
+    datasets: [
+      {
+        data: [15, 20, 30, 10],
+        backgroundColor: ["#3498DB", "#9B59B6", "#E74C3C", "#26B99A"],
+        hoverBackgroundColor: ["#49A9EA", "#B370CF", "#E95E4F", "#36CAAB"]
+      }
+    ]
+  },
+  options: { responsive: true }
+});
